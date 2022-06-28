1bf427730e0a9196e4e1402076e697de9331734b
doiboroi <doiboroi@users.noreply.github.com>
2022-06-28 18:13:37 +0700

    Update t.js

--- t.js
+++ t.js
@@ -1,5 +1,5 @@
 // conan_kinhmat
-var iVersion = "3.002";
+var iVersion = "3.003";
 var iIgnoreTime = parseInt( localStorage.getItem("ignore-time"), 10 );
 
 var bAuto2 = false;
@@ -1503,7 +1503,7 @@ function step_1(){
         
         // console.log('id: ' + $iChannelID )
         // console.dir( 'https://live.shopee.vn/universal-link/middle-page?type=live&id='+$iChannelID+'&deep_and_deferred=1&room_title='+sRoomTitle+'&room_host='+sRoomHost+'&room_coin='+sRoomCoin+'&room_view='+sRoomViews+'&acu='+accu+'#share' )
-        window.location.href = 'https://live.shopee.vn/universal-link/middle-page?type=live&id='+$iChannelID+'&deep_and_deferred=1&room_title='+sRoomTitle+'&room_host='+sRoomHost+'&room_coin='+sRoomCoin+'&room_view='+sRoomViews+'&acu='+accu+'#share'
+        
 
         if( sRoomCoin != "N/A" ){
             localStorage.setItem('pause-auto', true)
@@ -1517,6 +1517,8 @@ function step_1(){
                 window.location.href = window.location.href
             },3000)
         }
+
+        window.location.href = 'https://live.shopee.vn/universal-link/middle-page?type=live&id='+$iChannelID+'&deep_and_deferred=1&room_title='+sRoomTitle+'&room_host='+sRoomHost+'&room_coin='+sRoomCoin+'&room_view='+sRoomViews+'&acu='+accu+'#share'
 
     }
 }
